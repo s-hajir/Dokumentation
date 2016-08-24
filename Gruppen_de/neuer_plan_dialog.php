506692509a38da407a127e5c0757cf3180353796
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-08-24 15:17:18 +0200

    Update neuer_plan_dialog.php

--- Gruppen_de/neuer_plan_dialog.php
+++ Gruppen_de/neuer_plan_dialog.php
@@ -5,7 +5,7 @@
         <input type="text" id="titel" name="titel"/> <br>                                <!--Input Titel -->
         <label for="datum"></label><br>                                                  <!--Label für Datum -->
         <input type="date" id="datum" name="datum"/> <br>                                <!--Input Datum -->
-        <?php include "freischalten_markieren_freunde.php" ?>                            <!-- Bereich: Freischalten für Freunde/Markieren von Freunden--> 
+        <?php include "freischalten_markieren_freunde.php" ?>                            <!-- Bereich: Freischalten für Freunde/Markieren von Freunden. Name des Dokumentes entspricht nicht buchstabengenu dem Namen der Gruppe--> 
         <button type="submit" id="Erstelle">Erstelle</button>                            <!--Button schickt Formular ab -->
         <button type="button" id="Zurueck">Zurueck</button>                              <!--Button schickt Formular nicht ab -->
       </form>
