9e519e02315b5a620ee58d95bae853d16d5f9201
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-08-24 14:16:25 +0200

    Create navigation.php

---
+++ Gruppen_de/navigation.php
@@ -0,0 +1,28 @@
+<nav>                                                                                   <!--Navigations-Bereich-->
+    	<ul>                                                                                  <!--Navigation besteht aus Liste von Links-->
+    		<li><a href="index.php"><img src="logo.gif"></a></li>                              <!--Logo-->
+    		<li>                                                                                <!--Such-Formular-Bereich-->
+    		  <form>                                                                            <!--Suche nach Tasks/Plänen-->
+    		      <input id="navbar-suche" type="search" value="Suche...">
+    		      <button type="submit">Suchen</button>                                         
+    		  </form>
+    		</li>
+    		<li><button id="Neuer-Plan">+ neuer Plan</button></li>                          <!--startet Dialog für Erstellung eines neuen Planes-->
+    		<li><a href="#">Mehr</a>                                                        <!--Dropdown-Liste -->                                                                    
+    				<ul>
+    					<li><a href="#">Tagesplan</a></li>
+    					<li><a href="#">Feed</a></li>
+    					<li><a href="#">Freunde und Chat</a></li>
+    					<li><a href="#">Übersicht</a></li>
+    				</ul>
+    		</li>
+    		<li><a href="#" class="notifikation" data-badge="5">Notifikationen</a></li>     <!--Notifikationen mit einem Badge. Badge gibt Anzahl der Notifikationen an -->
+    		<li><a href="#">Profil</a>
+    		    <ul>
+    					<li><a href="#">Dein Profil</a></li>
+    					<li><a href="#">Einstellungen</a></li>
+    					<li><a href="#">Ausloggen</a></li>
+    				</ul>
+    		</li>
+    	</ul>
+    </nav>
