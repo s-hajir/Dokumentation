9b168306f7f874eb7d0f2f3ed208555303e64ef6
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-08-24 14:44:30 +0200

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -7,49 +7,10 @@
 </head>
 <body>
   <header> 
-    <nav>                                                                                   <!--Navigations-Bereich-->
-    	<ul>                                                                                  <!--Navigation besteht aus Liste von Links-->
-    		<li><a href="index.html"><img src="logo.gif"></a></li>                              <!--Logo-->
-    		<li>                                                                                <!--Such-Formular-Bereich-->
-    		  <form>                                                                            <!--Suche nach Tasks/Plänen-->
-    		      <input id="navbar-suche" type="search" value="Suche...">
-    		      <button type="submit">Suchen</button>                                         
-    		  </form>
-    		</li>
-    		<li><button id="Neuer-Plan">+ neuer Plan</button></li>                          <!--startet Dialog für Erstellung eines neuen Planes-->
-    		<li><a href="#">Mehr</a>                                                        <!--Dropdown-Liste -->                                                                    
-    				<ul>
-    					<li><a href="#">Tagesplan</a></li>
-    					<li><a href="#">Feed</a></li>
-    					<li><a href="#">Freunde und Chat</a></li>
-    					<li><a href="#">Übersicht</a></li>
-    				</ul>
-    		</li>
-    		<li><a href="#" class="notifikation" data-badge="5">Notifikationen</a></li>     <!--Notifikationen mit einem Badge. Badge gibt Anzahl der Notifikationen an -->
-    		<li><a href="#">Profil</a>
-    		    <ul>
-    					<li><a href="#">Dein Profil</a></li>
-    					<li><a href="#">Einstellungen</a></li>
-    					<li><a href="#">Ausloggen</a></li>
-    				</ul>
-    		</li>
-    	</ul>
-    </nav>
+    <?php include "navigation.php" ?>
   </header>
   <main>
-    <dialog id="dialog">                                                                 <!--Dialog Bereich. Wird angezeigt bei Klick auf "+neuer plan" --> -->
-      <h2>Neuen Plan erstellen</h2>
-      <form action="tagesplan.php" method="get">                                         <!--Bereich Neuer-Plan-Formular -->
-        <label for="titel"></label><br>                                                  <!--Label für Titel -->
-        <input type="text" id="titel" name="titel"/> <br>                                <!--Input Titel -->
-        <label for="datum"></label><br>                                                  <!--Label für Datum -->
-        <input type="date" id="datum" name="datum"/> <br>                                <!--Input Datum -->
-        <!--<section>Freischalten/Markieren</section>-->                                 <!-- Bereich: Freischalten für Freunde/Markieren von Freunden--> 
-        <button type="submit" id="Erstelle">Erstelle</button>                            <!--Button schickt Formular ab -->
-        <button type="button" id="Zurueck">Zurueck</button>                              <!--Button schickt Formular nicht ab -->
-      </form>
-    </dialog>
-    
+    <?php include "neuer_plan_dialog.php" ?>
     
     <section>                                                                             <!--Bereich Login-Registrieren-Formular -->
     <form action="login.php" method="get">                                                <!--Login-Formular -->
@@ -80,7 +41,7 @@
       
       function validatePassword(){         
         if(checkPassword(password.value)){                                                 //check Passwortformat-und-Länge
-            if(password.value != confirm_password.value) {                                 //check ob Passwörter nicht überinstimmen
+            if(password.value != confirm_password.value) {                                 //check Passwort Gleichheit
               confirm_password.setCustomValidity("Passwoerter stimmen nicht ueberein");
             } else {confirm_password.setCustomValidity('');}
             
@@ -95,7 +56,7 @@
       confirm_password.onkeyup = validatePassword;
   </script>
   
-  <script>                                                  //öffnet und schließt Dialog
+  <script>                                                  //öffnet und schließt Dialog für neue Plan-Erstellung
      var startbutton = document.getElementById("Neuer-Plan"),
       dialog = document.getElementById('dialog'),
       erstellebutton = document.getElementById("Erstelle"),
