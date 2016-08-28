35cc0ad2bcf88acd71e62a388b1799a2a6a8ea1b
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-08-28 16:16:18 +0200

    Create form_eval_register.php

---
+++ Formulare/form_eval_register.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+define('DB_SERVER', "localhost");
+define('DB_USER', "shahir");
+define('DB_PASSWORD', "toor");
+define('DB_DATABASE', "webapp");
+define('DB_DRIVER', "mysql");
+
+function getDbConnection() {
+    $db = new PDO(DB_DRIVER . ":dbname=" . DB_DATABASE . ";host=" . DB_SERVER, DB_USER, DB_PASSWORD);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $db;
+}
+
+function validateRegistration() {
+    $username = $_GET['username'];
+    $db = getDbConnection();
+    $stmt = $db->prepare("SELECT username FROM `appuser` WHERE username='".$username."'");
+    $isQueryOk = $stmt->execute();
+    $results = array();
+    if ($isQueryOk) {
+        $results = $stmt->fetchAll();
+        if(empty($results)){
+            $firstname = $_GET['firstname'];
+            $lastname = $_GET['lastname'];
+            $password = $_GET['password'];
+
+            $stmt2 = $db->prepare("INSERT INTO `appuser` (`id`, `vName`, `nName`, `imgUrlProfil`, `username`, `passwort`) VALUES (NULL, '".$firstname."', '".$lastname."', '".$firstname."_".$lastname."(".$username.")/".$firstname."_".$lastname."(".$username.")_profil.jpg', '".$username."', '".$password."');");
+            $isQueryOk2 = $stmt2->execute();
+            if ($isQueryOk2){
+                echo "<form action='tagesplan.php' method='get'><h2>Willkommen! ".$username."</h2><p style='color:green;'>Hier gehts weiter</p><br><button type='submit' style='color:green;' >Weiter</button></form>";
+                $_SESSION['name'] = $firstname." ".$lastname;
+                $_SESSION['username'] = $username;
+            }else{
+                echo "<p style='color:red;'>Datenbank-Problem beim Anlegen des Accounts</p>";
+            }
+
+        }else{ //username gibt es in der Tabelle schon
+            echo "<p style='color:red;'>Fehler: Der Nutzername existiert schon</p>";
+        }
+    } else {
+        trigger_error('Error executing statement.', E_USER_ERROR);
+        echo "<p style='color:red;'>Datenbank-Problem</p>";
+    }
+    //Verbindung beenden
+    $db = null;
+}
+
+validateRegistration();
+
+?>
