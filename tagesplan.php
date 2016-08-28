d644503effa46af74caf60e6ca529e12fa86a7f0
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-08-28 14:41:33 +0200

    Create tagesplan.php

---
+++ tagesplan.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="de">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ProjektA</title>
+    <link rel="stylesheet" href="jquery-ui.css" />
+    <link rel="stylesheet" href="jquery-ui.theme.css" />
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
+
+</head>
+<body>
+    <header>
+        <img src="logo.jpg" />
+    </header>
+    <main>
+        <?php include "navigationsleiste.php";  ?>  <!---->
+        <?php include "neuer_plan_dialog.php";  ?>  <!--include freischalten_markieren_freunde.html + JS für Dialog-->
+        <section>
+        </section>
+    </main>
+    <footer>&copy; Copyright 2016 Hajir</footer>
+</body>
+</html>
