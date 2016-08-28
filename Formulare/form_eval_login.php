ebcdd8a536f2591c7abace78756a9f3f14560764
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-08-28 16:11:43 +0200

    Create form_eval_login.php

---
+++ Formulare/form_eval_login.php
@@ -0,0 +1,42 @@
+<?php
+
+session_start();
+
+define('DB_SERVER', "localhost");
+define('DB_USER', "shahir");
+define('DB_PASSWORD', "toor");
+define('DB_DATABASE', "webapp");
+define('DB_DRIVER', "mysql");
+
+function getDbConnection() {
+    $db = new PDO(DB_DRIVER . ":dbname=" . DB_DATABASE . ";host=" . DB_SERVER, DB_USER, DB_PASSWORD);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $db;
+}
+
+function validateLogin() {
+    $username = $_GET['username'];
+    $password = $_GET['password'];
+    $db = getDbConnection();
+    $stmt = $db->prepare("SELECT CONCAT(vName,' ', nName) As name, username, passwort FROM `appuser` WHERE username='".$username."' AND passwort='".$password."'");
+    $isQueryOk = $stmt->execute();
+    $results = array();
+
+    if ($isQueryOk) {
+        $results = $stmt->fetchAll(PDO::FETCH_COLUMN,0);  //Nur erste Spalte zurück als Array
+        if(!empty($results)){
+            echo "<form action='tagesplan.php' method='get'><h2>Willkommen! ".$username."</h2><p style='color:green;'>Hier gehts weiter</p><br><button type='submit' style='color:green;' >Weiter</button></form>";
+            $_SESSION['name']= $results[0];
+            $_SESSION['username'] =$username;
+
+        }else{echo "<p style='color:red;'>Nutzername/Passwort falsch ! Bitte nochmal versuchen</p>";}
+    } else {
+        trigger_error('Error executing statement.', E_USER_ERROR);
+        echo "<p style='color:red;'>Datenbank-Problem beim Anlegen des Accounts</p>";
+    }
+    //Verbindung beenden
+    $db = null;
+}
+
+validateLogin();
+?>
