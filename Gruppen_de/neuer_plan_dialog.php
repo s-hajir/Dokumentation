d8f54762032c621a19c4e3f54cf19999117da13e
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-09-01 21:54:33 +0200

    Update neuer_plan_dialog.php

--- Gruppen_de/neuer_plan_dialog.php
+++ Gruppen_de/neuer_plan_dialog.php
@@ -1,35 +1,19 @@
-    <dialog id="dialog">                                                                 <!--Dialog Bereich. Wird angezeigt bei Klick auf "+neuer plan" --> -->
-      <h2>Neuen Plan erstellen</h2>
-      <form action="tagesplan.php" method="get">                                         <!--Bereich Neuer-Plan-Formular -->
-        <label for="titel"></label><br>                                                  <!--Label für Titel -->
-        <input type="text" id="titel" name="titel"/> <br>                                <!--Input Titel -->
-        <label for="datum"></label><br>                                                  <!--Label für Datum -->
-        <input type="date" id="datum" name="datum"/> <br>                                <!--Input Datum -->
-        <?php include "freischalten_markieren_freunde.html" ?>                            <!-- Bereich: Freischalten für Freunde/Markieren von Freunden. Name des Dokumentes entspricht nicht buchstabengenu dem Namen der Gruppe--> 
-        <button type="submit" id="Erstelle">Erstelle</button>                            <!--Button schickt Formular ab -->
-        <button type="button" id="Zurueck">Zurueck</button>                              <!--Button schickt Formular nicht ab -->
-      </form>
-    </dialog>
-    <script>
-         //öffnet und schliesst Dialog für neue Plan-Erstellung
-         var startbutton = document.getElementById("Neuer-Plan"),
-         dialog = document.getElementById("dialog"),
-         erstellebutton = document.getElementById("Erstelle"),
-         zurueckbutton = document.getElementById("Zurueck");
-         startbutton.addEventListener('click', zeigeFenster);
-         erstellebutton .addEventListener('click', schliesseFenster);
-         zurueckbutton .addEventListener('click', schliesseFenster);
-
-         function schliesseFenster() {
-          dialog.close();
-          document.getElementById("tags").value="";                    //Wert & img_container leeren
-          var img_container = document.getElementById("img_container");
-          while (img_container.firstChild) {                                    
-              img_container.removeChild(img_container.firstChild);
-          }
-         }
-         function schliesseFenster2() {
-                                                                      //Verbesserung: AJAX -> Server macht DB Eintrag -> JSON String zurück "Erfolgreich"/"Fehlgeschlagen"
-             dialog.close();
-         }
-    </script>
+<dialog id="dialog">
+    <!--Dialog Bereich. Wird angezeigt bei Klick auf "+neuer plan" -->-->
+    <h2>Neuen Plan erstellen</h2>
+    <form id="form_neuer_plan_dialog" action="tagesplan2.php" method="get">
+        <!--Bereich Neuer-Plan-Formular -->
+        <label for="titel">Titel</label>
+        <br /><!--Label für Titel -->
+        <input type="text" id="titel" name="titel" />
+        <br /><!--Input Titel -->
+        <label for="datum">Datum</label>
+        <br /><!--Label für Datum -->
+        <input type="date" id="datum" name="datum" />
+        <br /><!--Input Datum -->
+        <?php include "freischalten_markieren_freunde.html" ?><!-- Bereich: Freischalten für Freunde/Markieren von Freunden. Name des Dokumentes entspricht nicht buchstabengenu dem Namen der Gruppe-->
+        <br>
+        <button type="submit" id="Erstelle">Erstelle</button><!--Button schickt Formular ab -->
+        <button type="button" id="Zurueck">Zurueck</button><!--Button schickt Formular nicht ab -->
+    </form>
+</dialog>
