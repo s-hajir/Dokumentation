8200258c540d538a9626e8201be10196df2e5380
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-09-02 23:57:40 +0200

    Update form_eval_profil_einstellung.php

--- Formulare/form_eval_profil_einstellung.php
+++ Formulare/form_eval_profil_einstellung.php
@@ -1,1 +1,4 @@
-<!--Aufgerufen von einstellungen.php->uploadProfilbild-->
+<!--Aufgerufen von einstellungen.php->uploadProfilbild
+1.über ($_SESSION['username'])  Nutzernamen holen
+2.mit (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],"Zielverzeichnis/Dateiname")) das Profilbild im Verzeichnis des Nutzers ablegen
+3.mit (echo "<img src='BildUrl' width='150' height='140'></img>") ein img-Element an Client schicken-->
