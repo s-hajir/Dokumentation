87febd774485a138eb744ac0326bd9ba98526b78
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-09-02 12:13:26 +0200

    Update werbung.php

--- Gruppen_de/werbung.php
+++ Gruppen_de/werbung.php
@@ -1,1 +1,8 @@
-<!-- Werbung -->
+<section>                                                             <!--Bereich für Werbung-->
+  <figure>                                                            <!--Container für Werbung-->
+    <img src="" width="100" height="500">                             <!--Werbebild/foto. Wird später dynamisch erzeugt.-->
+    <figcaption>                                                      <!--Text zum Bild/Foto. Wird später dynamisch erzeugt.-->
+      <a href="">Werbetext</a>                                        <!--Verweis auf die Seite, für die geworben wird-->
+    </figcaption>
+    </figure>
+</section>
