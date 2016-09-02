6611bf0db237ebc9845c64c0fca674a74798ecb6
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-09-02 14:23:35 +0200

    Update neuer_plan_dialog.php

--- Gruppen_de/neuer_plan_dialog.php
+++ Gruppen_de/neuer_plan_dialog.php
@@ -1,19 +1,18 @@
 <dialog id="dialog">
-    <!--Dialog Bereich. Wird angezeigt bei Klick auf "+neuer plan" -->-->
+    <!--Dialog Bereich. Wird angezeigt bei Klick auf "+neuer plan" in der Navigationsleiste-->
     <h2>Neuen Plan erstellen</h2>
-    <form id="form_neuer_plan_dialog" action="form_eval_neuer_plan_dialog.php" method="get">
-        <!--Bereich Neuer-Plan-Formular -->
-        <label for="titel">Titel</label>
-        <br /><!--Label für Titel -->
-        <input type="text" id="titel" name="titel" />
-        <br /><!--Input Titel -->
-        <label for="datum">Datum</label>
-        <br /><!--Label für Datum -->
-        <input type="date" id="datum" name="datum" />
-        <br /><!--Input Datum -->
-        <?php include "freischalten_markieren_freunde.html" ?><!-- Bereich: Freischalten für Freunde/Markieren von Freunden. Name des Dokumentes entspricht nicht buchstabengenu dem Namen der Gruppe, welche im 'strukt.html' steht-->
+    <form id="form_neuer_plan_dialog" action="form_eval_neuer_plan_dialog.php" method="get"> <!--Formular zum Erstellen eines neuen Planes -->
+        <label for="titel">Titel</label>                                                     <!--Label für Titel-->
+        <br />
+        <input type="text" id="titel" name="titel" />                                        <!--Input Titel(optional)-->
+        <br />
+        <label for="datum">Datum</label>                                                     <!--Label für Datum -->
+        <br />
+        <input type="date" id="datum" name="datum" required="required" />                    <!--Input Datum -->
+        <br />
+        <?php include "freischalten_markieren_freunde.html" ?>                               <!-- Bereich: Freischalten für Freunde/Markieren von Freunden. Name des Dokumentes entspricht nicht buchstabengenu dem Namen der Gruppe, welche im 'strukt.html' steht-->
         <br>
-        <button type="submit" id="Erstelle">Erstelle</button><!--Button schickt Formular ab -->
-        <button type="button" id="Zurueck">Zurueck</button><!--Button schickt Formular nicht ab -->
+        <button type="submit" id="Erstelle">Erstelle</button>                                <!--Button schickt Formular ab und schließt modalen Dialog -->
+        <button type="button" id="Zurueck">Zurueck</button>                                  <!--Button schließt modalen Dialog-->
     </form>
 </dialog>
