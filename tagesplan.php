5afe9129996e8a298bb5c5dbefe3f3d67d7f172d
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-09-03 15:20:25 +0200

    Update tagesplan.php

--- tagesplan.php
+++ tagesplan.php
@@ -41,7 +41,7 @@
                 <br />
                 <?php include "freischalten_markieren_freunde.html" ?>                                 <!-- Bereich: Freischalten für Freunde/Markieren von Freunden-->
                 <br />
-                <button type="submit" id="ErstelleTask">Erstelle</button>                                  <!--Button schickt Formular ab und schließt modalen Dialog -->
+                <input type="submit" id="ErstelleTask" value="Erstelle"/>                                  <!--Button schickt Formular ab und schließt modalen Dialog -->
                 <button type="button" id="ZurueckTask">Zurueck</button>                                    <!--Button schließt modalen Dialog-->
             </form>
         </dialog>
