8377751edee347a6f6d247b1a9e9e13847329541
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-09-16 02:10:29 +0200

    Nutzer kann nun neues Profilbild hochladen (das alte wird überschrieben)

--- Formulare/form_eval_profil_einstellung.php
+++ Formulare/form_eval_profil_einstellung.php
@@ -1,4 +1,22 @@
-<!--Aufgerufen von einstellungen.php->uploadProfilbild
-1.über ($_SESSION['username'])  Nutzernamen holen
-2.mit (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],"Zielverzeichnis/Dateiname")) das Profilbild im Verzeichnis des Nutzers ablegen
-3.mit (echo "<img src='BildUrl' width='150' height='140'></img>") ein img-Element an Client schicken-->
+<!--Aufgerufen von einstellungen.php->uploadProfilbild -->
+
+<?php
+session_start();
+
+if($_FILES["fileToUpload"]["error"] == 0) { //falls Upload OK ist Ergebnis 0 (falls kein File-Upload -> 4, falls File zu groß -> 1)
+    $fullname = $_SESSION['name'];
+    $fullnameNoSpace = str_replace(" ","_",$fullname);
+    $username = $_SESSION['username'];
+    $imgUrl = $_SESSION['imgUrl'];
+
+    //Falls es kein Verzeichnis für diesen Nutzer gibt -> lokal anlegen
+    if(!is_dir($fullnameNoSpace)){
+        mkdir($fullnameNoSpace);
+    }
+    
+    //File ins Verzeichnis rein
+    $erfolg = move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $imgUrl); 
+    echo "<figcaption>Bild wurde veraendert, bitte Seite neu laden </figcaption>";
+    }
+?>
+
