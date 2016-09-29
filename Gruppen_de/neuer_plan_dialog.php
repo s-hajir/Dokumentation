6e87ac1675b81b542f76b5fddf6ef9367111d384
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-09-29 15:51:22 +0200

    Form-tag wieder hinzugefügt
    Grund: über das Form-tag lassen sich die Input-Felder mit "reset()" leicht zurücksetzen, Felder müssen nicht manuell geleert werden

--- Gruppen_de/neuer_plan_dialog.php
+++ Gruppen_de/neuer_plan_dialog.php
@@ -1,6 +1,7 @@
 <dialog id="dialog">
     <!--Dialog Bereich. Wird angezeigt bei Klick auf "+neuer plan" in der Navigationsleiste-->
     <h2>Neuen Plan erstellen</h2>                                                            <!--Überschrift-->
+      <form id="form_neuer_plan_dialog">
         <label for="titel">Titel</label>                                                     <!--Label für Titel-->
         <br />
         <input type="text" id="titel" name="titel" />                                        <!--Input Titel(optional)-->
@@ -13,4 +14,5 @@
         <br>
         <input type="submit" id="Erstelle" value="Erstelle" />                               <!--Button schickt Formular ab und schließt modalen Dialog -->
         <input type="button" id="Zurueck" value="Zurueck"/>                                  <!--Button schließt modalen Dialog-->
+      </form>
 </dialog>
