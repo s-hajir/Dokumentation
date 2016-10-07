ebdd9e0c656767e439056a9cb92f2fafd8a62c7e
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-10-07 14:13:43 +0200

    alle freigegebene Nutzer in Tabelle `berechtigtenutzertp` eintragen
    Schaltet der Nutzer seinen Plan für Freunde frei -> Werden diese Freunde in die Tabelle `berechtigtenutzertp` zeilenweise eingetragen.
    Später soll anhand dieser Eintragung überprüft werden, ob ein anderer Nutzer Zugriff auf diesen Tagesplan hat.

--- Formulare/form_eval_neuer_plan_dialog.php
+++ Formulare/form_eval_neuer_plan_dialog.php
@@ -15,30 +15,45 @@ if(isset($_GET['datum']) && !empty($_GET['datum'])){
     $beschreibung = $_GET['beschreibung'];
     $username =  $_SESSION['username'];
     $iduser = "";
-
+	
+	//aus String -> Array mit String-Elementen bilden und dabei "," entfernen
+	$freundeArray = explode(',', $freischaltenFuer);
 
      $db = getDbConnection();
-     
+     //SELECT userid
      $stmt = $db->prepare("SELECT iduser FROM `appusers` WHERE username='".$username."'");
      $isQueryOk = $stmt->execute();
      if ($isQueryOk) {
             $results = $stmt->fetchAll(PDO::FETCH_COLUMN,0); 
             $iduser = $results[0];
 
-            
-            $stmt2 = $db->prepare("INSERT INTO `tagesplaene` (`idTp`, `beschreibung`, `datum`, `zeitstempel`, `iduser`, `bezeichnung`) VALUES (NULL, '".$beschreibung."', '".$datum."',NULL , '".$iduser."', '".$titel."');");
+            //INSERT Plan + SELECT idTp des neu erzeugten Planes
+            $stmt2 = $db->prepare("INSERT INTO `tagesplaene` (`idTp`, `beschreibung`, `datum`, `zeitstempel`, `iduser`, `bezeichnung`) VALUES (NULL, '".$beschreibung."', '".$datum."',NULL , '".$iduser."', '".$titel."'); ");
             $isQueryOk2 = $stmt2->execute();
+			$stmt3 = $db->prepare("SELECT idTp FROM `tagesplaene` WHERE datum='".$datum."';" );
+			$stmt3->execute();
+         
              if ($isQueryOk2) {
-                    echo json_encode("<p style='color:green;'>Plan '".$titel."' erfolgreich angelegt");
-            }else {
+			//hole idTp des neu erzeugten Planes
+				 $resultsIdTp = $stmt3->fetchAll(PDO::FETCH_COLUMN,0); 
+				 $idTp = $resultsIdTp[0];
+             //INSERT alle bereichtigte Nutzer in Tabelle `berechtigtenutzertp`
+				$trimmedArray = array_map('trim', $freundeArray);
+				$freundeArray2 = array_filter($trimmedArray);
+				foreach($freundeArray2 AS $name) {
+						$stmt4 = $db->prepare("INSERT INTO `berechtigtenutzertp` (`idBerechtNTp`, `name`, `zeitstempel`, `idTp`) VALUES (NULL, '".$name."', NULL, '".$idTp."'); ");
+							$isQueryOk4 = $stmt4->execute();
+				}
+                    echo json_encode("<p style='color:green;'>Plan '".$titel."' erfolgreich angelegt. Die Plan-ID lautet: ".$idTp."");
+             }else {
                     trigger_error('Error executing statement.', E_USER_ERROR);
                     }
 
      }else{
         trigger_error('Error executing statement.', E_USER_ERROR);
      }
-    
-     $db = null;
+
+
 }else{
       echo json_encode("<p style='color:red;'>Fehler: Datum nicht angegeben</p>");
 }
