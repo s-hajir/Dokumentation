ae7b049e0accf943b81f0476bf84de14806f9e83
Shahir Hajir <s-hajir@users.noreply.github.com>
2016-10-30 20:48:44 +0100

    fieldset-Container im Formular ergänzt

--- Gruppen_de/neuer_plan_dialog.php
+++ Gruppen_de/neuer_plan_dialog.php
@@ -2,6 +2,7 @@
     <!--Dialog Bereich. Wird angezeigt bei Klick auf "+neuer plan" in der Navigationsleiste-->
     <h2>Neuen Plan erstellen</h2>                                                            <!--Überschrift-->
       <form id="form_neuer_plan_dialog">
+        <fieldset>
         <label for="titel">Titel</label>                                                     <!--Label für Titel-->
         <br />
         <input type="text" id="titel" name="titel" />                                        <!--Input Titel(optional)-->
@@ -18,5 +19,6 @@
         <br>
         <input type="submit" id="Erstelle" value="Erstelle" />                               <!--Button schickt Formular ab und schließt modalen Dialog -->
         <input type="button" id="Zurueck" value="Zurueck"/>                                  <!--Button schließt modalen Dialog-->
+        </fieldset>
       </form>
 </dialog>
